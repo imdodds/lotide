e8faa1c96c0f904d85d73e2279d1ff5ec3cd475e
Ian Dodds <ianmitchelldodds@gmail.com>
2022-07-13 20:27:16 +0000

    improved with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,9 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🚨🚨🚨 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("butt", "butt");
-assertEqual("fart", "poop");
-assertEqual(69, 69);
-assertEqual(69, 420);
