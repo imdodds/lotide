5237cb06c14d3dad6e1e8fdd3e949df05c02b9b9
Ian Dodds <ianmitchelldodds@gmail.com>
2022-07-20 20:11:09 +0000

    refactored eqArrays.js for simplicity and readability

--- eqArrays.js
+++ eqArrays.js
@@ -9,21 +9,19 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
 
-  let matches = [];
-
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        matches.push(array1[i]);
-      }
-    }
-  }
-
-  if (array1.length === matches.length) {
-    return true;
-  } else {
+  if (array1.length !== array2.length) {
     return false;
   }
+    
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([3, 2, 1], [2, 2]), false);
+assertEqual(eqArrays([61, 17, "fart", false], [61, 17, "fart", false]), true);
+assertEqual(eqArrays([5, 6, 7, 8], [8, 7, 6, 5]), false);
