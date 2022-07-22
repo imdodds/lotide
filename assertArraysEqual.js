1acd2164c756aa59eb8896ae8b09b6498cf5e6c0
Ian Dodds <ianmitchelldodds@gmail.com>
2022-07-22 15:15:57 +0000

    refactored assertArraysEqual function for simplicity and readability

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,16 @@
 const assertArraysEqual = function(array1, array2) {
 
-  let matches = [];
-
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        matches.push(array1[i]);
+  if (array1.length !== array2.length) {
+    return `🚨🚨🚨 Assertion Failed: ${array1} !== ${array2}`;
+  }
+    
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return `🚨🚨🚨 Assertion Failed: ${array1} !== ${array2}`;
       }
     }
-  }
+  return `✅✅✅ Assertion Passed: ${array1} === ${array2}`;
 
-  if (array1.length === matches.length) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🚨🚨🚨 Assertion Failed: ${array1} !== ${array2}`);
-  }
 };
 
 console.log(assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]));
