f487f7ac84a8606f4d14ae1210becea6c2ed64ab
Ian Dodds <ianmitchelldodds@gmail.com>
2022-07-26 16:18:35 +0000

    added function that takes in an object and a callback and returns the first key for which the callback returns a truthy value

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const findKey = (object, callback) => {
+
+  const objEntries = Object.entries(object);
+
+  for (let item of objEntries) {
+    if (callback(item[1])) {
+      return item[0];
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TESTING
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
