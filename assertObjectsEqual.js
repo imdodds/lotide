472ebe17c6befe8a97ae8a6e67e1fe3b9b5eed13
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-07 18:40:23 +0000

    refactored assertObjectsEqual and added module.exports

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays.js');
+
 const assertObjectsEqual = function(object1, object2) {
   
   const inspect = require('util').inspect;
@@ -24,34 +26,6 @@ const assertObjectsEqual = function(object1, object2) {
   }
   // return true;
   return `✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`;
-
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-    
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-// TESTING
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(assertObjectsEqual(ab, ba)); // true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(assertObjectsEqual(ab, abc)); // false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(assertObjectsEqual(cd, dc)); // true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertObjectsEqual(cd, cd2)); // false
+module.exports = assertObjectsEqual;
