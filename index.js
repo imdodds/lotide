abf2f35f27e899f319e7510eadbd1e8fc5c16b64
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-07 17:12:29 +0000

    added index.js file for referencing lotide library utility functions

---
+++ index.js
@@ -0,0 +1,9 @@
+const head = require('./head.js');
+const tail = require('./tail.js');
+const middle = require('./middle.js');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle
+};
