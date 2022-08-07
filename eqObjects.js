af3bf29c9f800780f378e1e84443e57d5101b720
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-07 18:33:19 +0000

    refactored eqObjects and added module.exports

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,5 @@
+const eqArrays = require('../eqArrays.js');
+
 const eqObjects = function(object1, object2) {
 
   const obj1Keys = Object.keys(object1);
@@ -8,7 +10,6 @@ const eqObjects = function(object1, object2) {
   }
     
   for (const key of obj1Keys) {
-
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
       } else if (object1[key] !== object2[key]) {
@@ -18,38 +19,4 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  console.log(array1.length, array2.length);
-  if (array1.length !== array2.length) {
-    return false;
-  }
-    
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-        return false;
-    }
-  }
-  return true;
-};
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+module.exports = eqObjects;
