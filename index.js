a758b7af757f0c0c6222f737afebbe5b88e7182f
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-07 19:01:20 +0000

    added references to all utility functions in Lotide library

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+const assertEqual = require('./assertEqual.js');
 const head = require('./head.js');
 const tail = require('./tail.js');
+const eqArrays = require('./eqArrays.js');
+const assertArraysEqual = require('./assertArraysEqual.js');
+const without = require('./without.js');
+const flatten = require('./flatten.js');
 const middle = require('./middle.js');
+const countOnly = require('./countOnly.js');
+const countLetters = require('./countLetters.js');
+const letterPositions = require('./letterPositions.js');
+const findKeyByValue = require('./findKeyByValue.js');
+const eqObjects = require('./eqObjects.js');
+const assertObjectsEqual = require('./assertObjectsEqual.js');
+const map = require('./map.js');
+const takeUntil = require('./takeUntil.js');
+const findKey = require('./findKey.js');
 
 module.exports = {
+  assertEqual: assertEqual,
   head: head,
   tail: tail,
-  middle: middle
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual,
+  without: without,
+  flatten: flatten,
+  middle: middle,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  eqObjects: eqObjects,
+  assertObjectsEqual: assertObjectsEqual,
+  map: map,
+  takeUntil: takeUntil,
+  findKey: findKey
 };
